1d4e9194b6343ca84c3ee140c147537ac5f5df24
Rodrigo Salsa <rodrigosalsag@hotmail.com>
2023-10-17 18:03:27 -0300

    Novos códigos de estudos Ada

---
+++ src/main/java/pacote1/Funções.java
@@ -0,0 +1,13 @@
+package pacote1;
+
+public class Funções {
+    public static void main(String[] args) {
+        String nome = "Rodrigo Salas";
+        saudacao(nome);
+    }
+
+    private static void saudacao(String nome) {
+        System.out.println("Oi, " + nome);
+    }
+
+}
